df70f8ff599e0ccd5cf2828dbe4886289cf10e12
fenix23707 <fenix23707@gmail.com>
2022-11-12 08:36:20 +0300

    init project

---
+++ src/main/kotlin/by/vsu/admission/office/AdmissionOfficeApplication.kt
@@ -0,0 +1,11 @@
+package by.vsu.admission.office
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdmissionOfficeApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdmissionOfficeApplication>(*args)
+}
---
+++ src/test/kotlin/by/vsu/admission/office/AdmissionOfficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.vsu.admission.office
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdmissionOfficeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
